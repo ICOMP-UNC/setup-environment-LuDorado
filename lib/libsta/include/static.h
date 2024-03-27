88c93c5e96765525ac86f73840d722e337ac6bed
LuDorado <m.lourdes.dorado@alumnos.unc.edu.ar>
2024-03-26 22:55:34 -0300

    Libraries definition

---
+++ lib/libsta/include/static.h
@@ -0,0 +1,9 @@
+#pragma once
+/**
+ * static_lib_function
+ * Function defined in the static library
+ *
+ * @param msg: the message to be printed.
+ * @return The message size
+ */
+int static_lib_function(char* msg);
